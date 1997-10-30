ea84b0dde822f97440a67f3c715f2cd677867de2
Henrik Grubbström (Grubba) <grubba@grubba.org>
1997-10-31 00:09:01 +0100

    Added.
    Rev: extern/acconfig.h:1.1

---
+++ extern/acconfig.h
@@ -0,0 +1,13 @@
+/*
+ * $Id: acconfig.h,v 1.1 1997/10/30 23:09:01 grubba Exp $
+ *
+ * Config file for some of Roxen's external binaries.
+ *
+ * $Author: grubba $
+ */
+#ifndef EXTERN_CONFIG_H
+#define EXTERN_CONFIG_H
+
+@TOP@
+@BOTTOM@
+#endif /* EXTERN_CONFIG_H */
