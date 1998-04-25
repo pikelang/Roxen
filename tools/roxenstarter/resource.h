14d1bfa36c3622d55f5a559ac4c26a1fcfe77e89
Johan Schön <js@opera.com>
1998-04-25 03:59:02 +0200

    Added startup tool for NT.
    Rev: tools/roxenstarter/resource.h:1.1
    Rev: tools/roxenstarter/roxen.ico:1.1
    Rev: tools/roxenstarter/roxenstarter.cpp:1.1
    Rev: tools/roxenstarter/roxenstarter.dsp:1.1
    Rev: tools/roxenstarter/roxenstarter.dsw:1.1
    Rev: tools/roxenstarter/roxenstarter.h:1.1
    Rev: tools/roxenstarter/roxenstarter.rc:1.1
    Rev: tools/roxenstarter/status.h:1.1

---
+++ tools/roxenstarter/resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by shellfun.rc
+//
+#define IDOK2                           3
+#define RoxenPopup                      103
+#define IDI_ICON1                       104
+#define ID_CONFIG_DIALOG                105
+#define IDR_MAINFRAME                   128
+#define ID_SHELLFUN_ICON                143
+#define ID_SHELLNOFUN_ICON              144
+#define ROXEN_ICON                      300
+#define IDC_CHECK1                      1000
+#define IDC_CHECK2                      1001
+#define IDC_CHECK3                      1002
+#define IDC_CHECK4                      1003
+#define IDC_EDIT1                       1005
+#define IDC_BUTTON1                     1006
+#define IDC_EDIT2                       1007
+#define IDM_FILEINFO                    32772
+#define IDM_ADD                         32775
+#define IDM_REMOVE                      32776
+#define IDM_TASKBAR                     32777
+#define IDM_STATECHANGE                 32784
+#define IDM_REMOVEICON                  32785
+#define IDM_ADDICON                     32786
+#define ID_MENUITEM40001                40001
+#define ID_MENUITEM40002                40002
+#define ID_MENUITEM40004                40004
+#define ID_APP_ABOUT                    0xE140
+#define ID_APP_EXIT                     0xE141
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
