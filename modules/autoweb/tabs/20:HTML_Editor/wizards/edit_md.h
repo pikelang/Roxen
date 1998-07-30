1e1ae6b9e5fa0b7b876bbac559f3212757d62272
Marcus Wellhardh <wellhard@roxen.com>
1998-07-30 12:46:53 +0200

    *** empty log message ***
    Rev: modules/autoweb/tabs/20:HTML_Editor/wizards/edit_md.h:1.1
    Rev: modules/autoweb/tabs/20:HTML_Editor/wizards/edit_md.pike:1.3

---
+++ modules/autoweb/tabs/20:HTML_Editor/wizards/edit_md.h
@@ -0,0 +1,79 @@
+array describe_metadata_var(array in)
+{
+  return ({ "<font size=+1><b>"+in[0]+"</b></font>", 
+	    "<var name='"+in[1]+"' default='"+in[2]+"' "+
+	    (in[3]==3?"choices='"+in[5]+"' ":" ")+
+	    " type='"+
+ 	    ((in[3]==0)?"string":"")+
+ 	    (in[3]==1?"text":"")+
+ 	    (in[3]==3?"select":"")+
+	    "'>",
+	    ({ "<font size=-1><i>"+in[4]+"</i></font>" }) });
+}
+
+array get_content_types( object id, object wa )
+{
+  return indices( wa->name_to_type );
+}
+
+string get_content_type( string f, object id, object wa, mapping|void m )
+{
+  string ct;
+
+  if (m) {
+    ct = m[ "content_type" ];
+
+    if (wa->content_types[ ct ])
+      return wa->content_types[ ct ]->name;
+  }
+  if (f) {
+    ct = wa->get_md(id, f)[ "content_type" ];
+
+    if (wa->content_types[ ct ])
+      return wa->content_types[ ct ]->name;
+  }
+  return wa->content_types[ "text/html" ]->name;
+}
+
+
+string page_editmetadata( object id, string f)
+{
+  object wa = id->misc->wa;
+  
+  mapping m = wa->get_md(id, f);
+  foreach (glob( "__*", indices( m ) ), string s)
+    m_delete( m, s );
+  
+  array (string) templates = ({ "No template", "default.tmpl" });
+  
+  array rows = ({
+    ({ "Type", "meta_content_type", 
+       get_content_type(f, id, wa, m), 3,
+       " This is the type of the file. "
+       /* Normal for text-files is text/html,"
+	  " most images are image/gif or image/jpeg."*/,
+       get_content_types( id, wa ) * ","
+    }),
+    ({ "Template", "meta_template", m->template||"No template", 3,
+       " This is the template used on this page. You can see all templates "
+       "available under the 'templates' tab.", templates * ","
+    }),
+    ({ "Title", "meta_title", m->title||"No title", 0,
+       " This is the title of the page. Make sure that it accurately "
+       "describes it"
+    }),
+    ({ "Keywords", "meta_keywords", m->keywords||"", 0,
+       " Document keywords. These are primarily used when search-engines "
+       "are indexing the site."
+    }),
+    ({ "Description<p><br><br><br>", "meta_description", 
+       m->description||"\n", 1,
+       " Document description. this is also primarily used when "
+       "search-engines are indexing the site."
+    }),
+  });
+  
+  return "<b>File Metadata for "+id->variables->path+":</b><p>\n" + 
+    html_table(({ "Data", "Value", ({ "Description" }) }),
+	       Array.map(rows, describe_metadata_var));
+}
--- modules/autoweb/tabs/20:HTML_Editor/wizards/edit_md.pike
+++ modules/autoweb/tabs/20:HTML_Editor/wizards/edit_md.pike
@@ -3,26 +3,9 @@ inherit "wizard";
 
 constant name = "Edit Metadata";
 
-
 string page_0( object id )
 {
-#if 0
-  mapping md = id->misc->wa->get_md(id, id->variables->path);
-  string r = "";
-  r += "<b>File Metadata for "+id->variables->path+":</b><br>\n";
-  r += "<tablify nice cellseparator='|'>"
-       "Description|Value\n";
-  foreach(sort(indices(md)), string variable) {
-    r += "<b>"+variable+":|</b>"+md[variable]+"\n";
-  }
-  r += "</tablify>\n";
-  return r+"<br>"+"<b>Title for file: '" + id->variables->path + "'</b>"
-    "<p><b>Enter title:</b>"
-    "<var name=md_title type=string "
-    "size=40 default=\""+md->title+"\">";
-#endif
   return page_editmetadata( id , id->variables->path);
-
 }
 
 mixed wizard_done( object id )
@@ -34,10 +17,9 @@ mixed wizard_done( object id )
   foreach (glob( "__*", indices( old_md ) ), string s)
     md[ s ] = old_md[ s ];
   foreach (glob( "meta_*", indices( id->variables )), string s)
-    md[ replace(s-"meta_", "_", "-") ] = id->variables[ s ];
-  md[ "content_type" ] = wa->name_to_type[ md[ "content-type" ] ];
+    md[ s-"meta_" ] = id->variables[ s ];
+  md[ "content_type" ] = wa->name_to_type[ md[ "content_type" ] ];
   if (md[ "template" ] == "No template")
     m_delete( md, "template" );
   wa->save_md_file(id, id->variables->path, md);
-
 }
