1ebfc0d2dcecb8f5a66b934f35443dbd6147d49e
Per Hedbor <ph@opera.com>
1998-09-10 16:19:44 +0200

    Prototypes
    Rev: modules/mail/clientlayer.h:1.1

---
+++ modules/mail/clientlayer.h
@@ -0,0 +1,86 @@
+// -*- pike -*-
+
+class Common
+{
+  int get_serial();
+}
+
+class Mail
+{
+  inherit Common;
+  object mailbox;
+  object user;
+  string body();
+  Stdio.File body_fd();
+  mapping headers(int force);
+  multiset flags(int force);
+  void set_flag(string name);
+  void clear_flag(string name);
+  mixed set(string name, mixed to);
+  mixed get(string name);
+}
+
+class Mailbox
+{
+  inherit Common;
+  object user;
+  int rename(string to);
+  void delete();
+  string query_name(int force);
+
+  array(Mail) mails();
+  Mail add_mail(Mail m, int|void do_not_copy_the_flags);
+  void remove_mail(Mail m);
+
+  Mail low_create_mail( string bodyid, mapping headers );
+  Mail create_mail_from_fd( Stdio.File from );
+  Mail create_mail_from_data( string from );
+  Mail create_mail( MIME.Message from );
+
+}
+
+class User
+{
+  inherit Common;
+  array(Mailbox) mailboxes();
+  mixed set( string name, mixed to );
+  mixed get( string name );
+  Mailbox get_or_create_mailbox( string name );
+  Mailbox get_incoming();
+  Mailbox get_drafts();
+}
+
+
+// clientlayer->find_user(user)->get_incoming()->create_mail_from_fd( fd );
+
+class ClientLayer
+{
+  program(Mail)    Mail;
+  program(Mailbox) Mailbox;
+  program(User)    User;
+
+  User get_user( string username, string password );
+  User get_user_from_adress( string adress );
+
+  object get_cache_obj( program type, int id );
+
+  /* low level functions */
+  int authenticate_user(string username, string passwordcleartext);
+  int find_user( string username_at_host );
+  mapping(string:int)    list_mailboxes(int user);
+  mapping(string:string) list_mail(int mailbox_id);
+  mapping(string:mixed)  get_mail_headers(string message_id);
+  int    update_message_refcount(string message_id, int deltacount);
+  int    delete_mail(string mail_id);
+  int    create_user_mailbox(int user, string mailbox);
+  string create_message(mapping mess);
+  string get_mailbox_name(int mailbox_id);
+  int    delete_mailbox(int mailbox_id);
+  int    rename_mailbox(int mailbox_id, string newname);
+  int    remove_mailbox_from_mail(string message_id, int mailbox_id);
+  int    add_mailbox_to_mail(string message_id, int mailbox_id);
+  void   set_mail_flag(string mail_id, string flag);
+  void   delete_mail_flag(string mail_id, string flag);
+  multiset get_mail_flags(string mail_id);
+}
+
