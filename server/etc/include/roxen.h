b95110f5c7051d3ce429f7ad3224a46902e41061
Per Hedbor <ph@opera.com>
1999-11-06 09:28:44 +0100

    oups, small #endif nesting error
    Rev: server/etc/include/roxen.h:1.10

--- server/etc/include/roxen.h
+++ server/etc/include/roxen.h
@@ -1,11 +1,10 @@
-// $Id: roxen.h,v 1.9 1999/11/06 08:20:07 per Exp $
+// $Id: roxen.h,v 1.10 1999/11/06 08:28:44 per Exp $
 #ifndef _ROXEN_H_
 
 #define _ROXEN_H_
 #include <config.h>
 #define HOST_TO_IP 'H'
 #define IP_TO_HOST 'I'
-#endif
 
 #define perror	roxen_perror
 
