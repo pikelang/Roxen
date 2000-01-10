1180ce64cc3e05754b6f5cab872c25486f2d1821
Marcus Comstedt <marcus@mc.pp.se>
2000-01-10 21:32:27 +0100

    Added some variables to the request object.
    Rev: server/java/src/se/idonex/roxen/RoxenRequest.java:1.2

--- server/java/src/se/idonex/roxen/RoxenRequest.java
+++ server/java/src/se/idonex/roxen/RoxenRequest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: RoxenRequest.java,v 1.1 1999/12/19 00:26:01 marcus Exp $
+ * $Id: RoxenRequest.java,v 1.2 2000/01/10 20:32:27 marcus Exp $
  *
  */
 
@@ -7,8 +7,31 @@ package se.idonex.roxen;
 
 public class RoxenRequest {
 
-  public RoxenConfiguration conf;
-  public String prot, clientprot, method;
-  public String realfile, virtfile;
+  public final RoxenConfiguration conf;
+  public final String rawURL, prot, clientprot, method;
+  public final String realfile, virtfile, raw, query, notQuery, remoteaddr;
+  /* variables */
+  /* request_headers */
+  /* supports */
+  /* pragma */
+  /* auth */
+
+  RoxenRequest(RoxenConfiguration _conf, String _rawURL, String _prot,
+	       String _clientprot, String _method, String _realfile,
+	       String _virtfile, String _raw, String _query,
+	       String _notQuery, String _remoteaddr)
+  {
+    conf = _conf;
+    rawURL = _rawURL;
+    prot = _prot;
+    clientprot = _clientprot;
+    method = _method;
+    realfile = _realfile;
+    virtfile = _virtfile;
+    raw = _raw;
+    query = _query;
+    notQuery = _notQuery;
+    remoteaddr = _remoteaddr;
+  }
 
 }
