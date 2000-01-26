9803696a229b2c6a9a535a2d5a00820ce50b2c16
Marcus Wellhardh <wellhard@roxen.com>
2000-01-26 16:29:37 +0100

    Removed renderPopup. removed file parameter from renderLink. Smarter handling of targets in renderLink
    Rev: server/modules/tags/javascript_support/scripts/Render.js:1.2

--- server/modules/tags/javascript_support/scripts/Render.js
+++ server/modules/tags/javascript_support/scripts/Render.js
@@ -1,7 +1,7 @@
-function renderLink(text, action, file, base, target)
+function renderLink(text, action, base, target)
 {
   return "&nbsp;<a href='"+base+"?action="+action+"' "+
-    (target != undefined?" target='"+target+"' ":"")+
+    (target != ""?" target='"+target+"' ":"")+
     "onMouseOver=\"setStatus('"+text+"');\" "+
     "onMouseOut=\"setStatus('');\""+
     ">"+text+"</a><br>\n";
@@ -18,69 +18,8 @@ function renderIcon(id, icon, text)
   return "<div id='"+id+"'><img border=0 src='"+icon+"' alt=''>"+text+"</div>";
 }
 
-function renderPopup(id, file, base, actions)
+function doDrop()
 {
-  var r =
-    "<div id='"+id+"' onMouseOut=\"hidePopup('"+id+"');\">\n<tab"+
-    "le border=1 cellspacing=1 cellpadding=3 bgcolor='bbbbbb'><tr><td>\n"+
-    "<b>"+file+"</b><hr>\n"+
-    "<i>View</i><br>\n";
-  if(actions>>0&1)
-    r += renderLink("View", "view.pike", file, base, "RoxenSBView");
-  if(actions>>1&1)
-    r += renderLink("View source", "view_source.pike",file,base,"RoxenSBView");
-  if(actions>>2&1)
-    r += renderLink("Log", "log.pike", file, base);
-  if(actions>>3&1)
-    r += renderLink("Annotate", "annotate.pike", file, base);
-  r += "<i>Edit</i><br>\n";
-  if(actions>>4&1)
-    r += renderLink("Edit", "edit_file, base.pike", file, base);
-  if(actions>>5&1)
-    r += renderLink("Edit new file", "create_file.pike", file, base);
-  if(actions>>6&1)
-    r += renderLink("Edit metadata", "edit_metadata.pike", file, base);
-  if(actions>>7&1)
-    r += renderLink("Download", "download.pike", file, base);
-  if(actions>>8&1)
-    r += renderLink("Upload file", "upload_file.pike", file, base);
-  if(actions>>9&1)
-    r += renderLink("Upload new file", "upload_new_file.pike", file, base);
-  r += "<i>File</i><br>\n";
-  if(actions>>10&1)
-    r += renderLink("Create directory", "create_dir.pike", file, base);
-  if(actions>>11&1)
-    r += renderLink("Copy", "copy.pike", file, base);
-  if(actions>>12&1)
-    r += renderLink("Move", "move.pike", file, base);
-  if(actions>>13&1)
-    r += renderLink("Delete", "delete.pike", file, base);
-  r += "<i>Version Control</i><br>\n";
-  if(actions>>14&1)
-    r += renderLink("Discard your changes", "discard.pike", file, base);
-  if(actions>>15&1)
-    r += renderLink("Update", "update.pike", file, base);
-  if(actions>>16&1)
-    r += renderLink("Commit", "commit.pike", file, base);
-  if(actions>>17&1)
-    r += renderLink("Join", "join.pike", file, base);
-  r += "<i>Access Control</i><br>\n";
-  if(actions>>18&1)
-    r += renderLink("Add protection point", "app_ppoint.pike", file, base);
-  if(actions>>19&1)
-    r += renderLink("Remove protection point", "remove_ppoint.pike",file,base);
-  r += "<i>Undelete</i><br>\n";
-  if(actions>>20&1)
-    r += renderLink("Enter normal mode","enter_normal_mode.pike",file, base);
-  if(actions>>21&1)
-    r +=renderLink("Enter undelete mode","enter_undelete_mode.pike",file,base);
-  if(actions>>22&1)
-    r += renderLink("Undelete", "undelete.pike", file, base);
-  r += "</td></tr></table></div>\n";
-  return r;
-}
-
-function doDrop() {
   if(modifiers & Event.CONTROL_MASK) {
     if(!(src_actions>>11&1)) {
       alert("Can not copy this file/directory");
