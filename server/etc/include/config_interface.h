f05204b3599b1aeb11e99df7edd6727b491325f8
Per Hedbor <ph@opera.com>
2000-02-02 01:21:10 +0100

    New config_setting macro
    Rev: server/etc/include/config_interface.h:1.2

--- server/etc/include/config_interface.h
+++ server/etc/include/config_interface.h
@@ -1,3 +1,4 @@
-#define config_setting(X) id->misc->config_settings->query(X)
+#define config_setting(X) (id->misc->config_settings->query(X))
+#define config_setting2(X) RXML.get_context()->get_var(X,"usr")
 #define config_perm(X)    id->misc->config_user->auth(X)
 #define config_host       id->misc->remote_config_host
