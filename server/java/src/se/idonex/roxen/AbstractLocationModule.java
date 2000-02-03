1fcecd14d5e27fd300c2bcdd788b52da84a81ed1
Marcus Comstedt <marcus@mc.pp.se>
2000-02-03 23:55:28 +0100

    Javadoc.
    Rev: server/java/src/se/idonex/roxen/AbstractLocationModule.java:1.2
    Rev: server/java/src/se/idonex/roxen/LocationModule.java:1.4
    Rev: server/java/src/se/idonex/roxen/Module.java:1.6

--- server/java/src/se/idonex/roxen/AbstractLocationModule.java
+++ server/java/src/se/idonex/roxen/AbstractLocationModule.java
@@ -1,27 +1,78 @@
 /*
- * $Id: AbstractLocationModule.java,v 1.1 2000/01/09 23:27:56 marcus Exp $
+ * $Id: AbstractLocationModule.java,v 1.2 2000/02/03 22:55:28 marcus Exp $
  *
  */
 
 package se.idonex.roxen;
 
-public abstract class AbstractLocationModule extends Module implements LocationModule {
+/**
+ * An abstract adaptor class that provides default implementations for
+ * most methods in the <code>LocationModule</code> interface.
+ * <P>
+ * A module inheriting this class must either create a module variable
+ * <code>location</code> using <code>defvar</code>, or provide a different
+ * implementation of the <code>queryLocation</code> method.
+ *
+ * @see LocationModule
+ * @see Module
+ *
+ * @version	$Version$
+ * @author	marcus
+ */
 
+public abstract class AbstractLocationModule extends Module implements LocationModule {
+  /**
+   * Returns the URL path handled by this module.
+   * Per default, this is the contents of the module variable <code>location</code>.
+   *
+   * @return the path name
+   */
   public String queryLocation()
   {
     return queryString("location");
   }
 
+  /**
+   * List the contents of a directory.
+   *
+   * @param  f   the path of the directory relative to the location of
+   *             this module
+   * @param  id  the request object
+   * @return     a list of filenames, or <code>null</code> if no such
+   *             directory exists.
+   */
   public String[] findDir(String f, RoxenRequest id)
   {
     return null;
   }
 
+  /**
+   * Get the real filename of a file.
+   *
+   * @param  f   the path of the file relative to the location of
+   *             this module
+   * @param  id  the request object
+   * @return     the path of the file in the host filesystem, or
+   *             <code>null</code> if this resource is not a real
+   *             file.
+   */
   public String realFile(String f, RoxenRequest id)
   {
     return null;
   }
 
+  /**
+   * Get the attributes of a file or directory.
+   * The attributes are a set of 7 integers.  These are: <code>mode</code>,
+   * <code>size</code>, <code>atime</code>, <code>mtime</code>,
+   * <code>ctime</code>, <code>uid</code>, <code>gid</code>.
+   * 
+   * @param  f   the path of the file or directory relative to the
+   *             location of this module
+   * @param  id  the request object
+   * @return     the attributes of this file or directory, or
+   *             <code>null</code> if this information is not available.
+   */
   public int[] statFile(String f, RoxenRequest id)
   {
     return null;
--- server/java/src/se/idonex/roxen/LocationModule.java
+++ server/java/src/se/idonex/roxen/LocationModule.java
@@ -1,16 +1,75 @@
 /*
- * $Id: LocationModule.java,v 1.3 2000/01/09 23:27:55 marcus Exp $
+ * $Id: LocationModule.java,v 1.4 2000/02/03 22:55:28 marcus Exp $
  *
  */
 
 package se.idonex.roxen;
 
+/**
+ * The interface for modules which have a specific URL path in the
+ * virtual file system.
+ *
+ * @see Module
+ *
+ * @version	$Version$
+ * @author	marcus
+ */
+
 public interface LocationModule {
 
+  /**
+   * Returns the URL path handled by this module.
+   *
+   * @return the path name
+   */
   String queryLocation();
+
+  /**
+   * Request a file from this module.
+   *
+   * @param  f   the path of the file relative to the location of
+   *             this module
+   * @param  id  the request object
+   * @return     a response, or <code>null</code> if no such
+   *             file exists.
+   */
   RoxenResponse findFile(String f, RoxenRequest id);
+
+  /**
+   * List the contents of a directory.
+   *
+   * @param  f   the path of the directory relative to the location of
+   *             this module
+   * @param  id  the request object
+   * @return     a list of filenames, or <code>null</code> if no such
+   *             directory exists.
+   */
   String[] findDir(String f, RoxenRequest id);
+
+  /**
+   * Get the real filename of a file.
+   *
+   * @param  f   the path of the file relative to the location of
+   *             this module
+   * @param  id  the request object
+   * @return     the path of the file in the host filesystem, or
+   *             <code>null</code> if this resource is not a real
+   *             file.
+   */
   String realFile(String f, RoxenRequest id);
+
+  /**
+   * Get the attributes of a file or directory.
+   * The attributes are a set of 7 integers.  These are: <code>mode</code>,
+   * <code>size</code>, <code>atime</code>, <code>mtime</code>,
+   * <code>ctime</code>, <code>uid</code>, <code>gid</code>.
+   * 
+   * @param  f   the path of the file or directory relative to the
+   *             location of this module
+   * @param  id  the request object
+   * @return     the attributes of this file or directory, or
+   *             <code>null</code> if this information is not available.
+   */
   int[] statFile(String f, RoxenRequest id);
 
 }
