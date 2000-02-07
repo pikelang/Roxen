ecadb5c67865e680d2aae10fa84d783d4dc9d9f6
Marcus Comstedt <marcus@mc.pp.se>
2000-02-08 00:25:29 +0100

    Removed some unneccesary flags.
    Rev: server/java/src/se/idonex/roxen/SimpleTagCaller.java:1.2

--- server/java/src/se/idonex/roxen/SimpleTagCaller.java
+++ server/java/src/se/idonex/roxen/SimpleTagCaller.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SimpleTagCaller.java,v 1.1 2000/02/07 17:00:56 marcus Exp $
+ * $Id: SimpleTagCaller.java,v 1.2 2000/02/07 23:25:29 marcus Exp $
  *
  */
 
@@ -27,21 +27,11 @@ public interface SimpleTagCaller {
   /** Never apply any prefix to this tag. */
   public static final int FLAG_NO_PREFIX = 0x00000002;
 
-  /** Declare the tag to be a socket tag, which accepts plugin tags. */
-  public static final int FLAG_SOCKET_TAG = 0x0000004;
-
   /** Don't preparse the content with the PHtml parser. */
   public static final int FLAG_DONT_PREPARSE = 0x00000040;
 
   /** Postparse the result with the PHtml parser. */
   public static final int FLAG_POSTPARSE = 0x00000080;
-
-  /** If set, the result will be interpreted in the scope of the
-   *  parent tag, rather than in the current one.                */
-  public static final int FLAG_PARENT_SCOPE = 0x00000100;
-
-  /** If set, the parser won't apply any implicit arguments. */
-  public static final int FLAG_NO_IMPLICIT_ARGS = 0x00000200;
 
   /** If set, the <tt>tagCalled</tt> method will be called repeatedly until
    *  it returns <tt>null</tt> or no more content is wanted.       */
@@ -59,31 +49,6 @@ public interface SimpleTagCaller {
 
   /** The same as specifying both FLAG_STREAM_RESULT and FLAG_STREAM_CONTENT */
   public static final int FLAG_STREAM = FLAG_STREAM_RESULT|FLAG_STREAM_CONTENT;
-
-  /** If set, the arguments to the tag need not be the same as
-   *  the cached args to enable caching.                        */
-  public static final int FLAG_CACHE_DIFF_ARGS = 0x00010000;
-
-  /** If set, the content need not be the same to enable caching. */
-  public static final int FLAG_CACHE_DIFF_CONTENT = 0x00020000;
-
-  /** If set, the result type need not be the same to enable caching. */
-  public static final int FLAG_CACHE_DIFF_RESULT_TYPE = 0x00040000;
-
-  /** If set, the variables with external scope need not have the
-   *  same values as the actual variables to enable caching.      */
-  public static final int FLAG_CACHE_DIFF_VARS = 0x00080000;
-
-  /** If set, the stack of call frames needs to be the same
-   *  to enable caching                                     */
-  public static final int FLAG_CACHE_SAME_STACK = 0x00100000;
-
-  /** 
-   * If set, the result will be stored in the frame instead of
-   * the final result. On a cache hit it'll be executed like the return
-   * value from <tt>tagCalled</tt> to produce the result.
-   */
-  public static final int FLAG_CACHE_EXECUTE_RESULT = 0x00200000;
 
 
   /**
