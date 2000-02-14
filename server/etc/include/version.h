f477d2ced25a55bcf109f4b641b7cd1198a7993e
Peter Bortas <zino@lysator.liu.se>
2000-02-15 00:18:21 +0100

    Version bumped to 2.0.4 by buildsystem.
    Rev: server/etc/include/version.h:1.7

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -3,7 +3,7 @@
  */
 
 constant __roxen_version__ = "2.0";
-constant __roxen_build__ = "3";
+constant __roxen_build__ = "4";
 
 #ifdef __NT__
 string real_version= "Roxen/"+__roxen_version__+"."+__roxen_build__+" NT";
