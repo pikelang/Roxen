fa4f79b96788c100ea8d0bc01bc9354c9a31b984
Martin Nilsson <mani@lysator.liu.se>
2000-03-13 03:15:36 +0100

    Stricter types and faster when not running REQUEST_DEBUG
    Rev: server/etc/include/request_trace.h:1.2

--- server/etc/include/request_trace.h
+++ server/etc/include/request_trace.h
@@ -1,2 +1,14 @@
-#define TRACE_ENTER(A,B) do{if(id->misc->trace_enter)id->misc->trace_enter((A),(B));}while(0)
-#define TRACE_LEAVE(A) do{if(id->misc->trace_leave)id->misc->trace_leave((A));}while(0)
+#ifdef REQUEST_DEBUG
+# define TRACE_ENTER(A,B) do{ \
+    if(([mapping(string:mixed)]id->misc)->trace_enter) \
+      ([function(string,mixed ...:void)]([mapping(string:mixed)]id->misc)->trace_enter)((A),(B)); \
+  }while(0)
+
+# define TRACE_LEAVE(A) do{ \
+    if(([mapping(string:mixed)]id->misc)->trace_leave) \
+      ([function(string:void)]([mapping(string:mixed)]id->misc)->trace_leave)((A)); \
+  }while(0)
+#else
+# define TRACE_ENTER(A,B)
+# define TRACE_LEAVE(A)
+#endif
