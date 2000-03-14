54bb6cda6211b4ee1d4a525e20a4db3824e42ce8
Marcus Wellhardh <wellhard@roxen.com>
2000-03-14 21:36:11 +0100

    Now layers are used as popups for netscape browsers instead of div. The layer is generated when the user clicks on the link instead of when the page is generated.
    Rev: server/modules/tags/javascript_support/scripts/Render.js:1.6

--- server/modules/tags/javascript_support/scripts/Render.js
+++ server/modules/tags/javascript_support/scripts/Render.js
@@ -45,3 +45,34 @@ function setPStat()
 {
   setStatus("Click on icon to raise action menu.");
 }
+
+// Function to possition a netscape layer.
+function bound_me_nicely()
+{
+  if( (this.left + this.clip.width - window.pageXOffset) > (window.innerWidth - 16) )
+    this.left = window.innerWidth - this.clip.width + window.pageXOffset - 16;
+  if( this.left < window.pageXOffset )
+    this.left = Math.max(0, window.pageXOffset);
+  if( (this.top + this.clip.height - window.pageYOffset) > (window.innerHeight) )
+    this.top = window.innerHeight - this.clip.height + window.pageYOffset;
+  if( this.top < window.pageYOffset )
+  {
+    this.top = Math.max(0, this.top);
+    window.scrollTo(window.pageXOffset,this.top);
+  }
+  //window.alert(this.width+'×'+this.height+'@'+this.left+'×'+this.top);
+  this.visibility = "show";
+}
+
+// Function to show the netscape layer named 'popup'.
+function showLayer(path, e)
+{
+  var l = document.layers['popup'];
+  l = document.layers['popup'];
+  l.top = e.target.y - 6;
+  l.left = e.target.x - 6;
+  l.onload = bound_me_nicely;
+  l.zIndex = 1;
+  l.src = path;
+  return false;
+}
