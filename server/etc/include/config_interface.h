8ece6af11b94b586729e7b3b49ac61f726f794c9
Per Hedbor <ph@opera.com>
2000-03-21 19:58:23 +0100

    More paranoia
    Rev: server/etc/include/config_interface.h:1.5

--- server/etc/include/config_interface.h
+++ server/etc/include/config_interface.h
@@ -1,6 +1,6 @@
 #define config_setting(X) (id->misc->config_settings->query(X))
 #define config_setting2(X) RXML.get_context()->get_var(X,"usr")
-#define config_perm(X)    id->misc->config_user->auth(X)
+#define config_perm(X)    (id->misc->config_user && id->misc->config_user->auth(X))
 #define config_host       id->misc->remote_config_host
 
 #define TYPE_THEME 254
