399d72c7d9c41529e26f06431221367f40978c00
Martin Nilsson <mani@lysator.liu.se>
2000-03-28 17:01:37 +0200

    Added comment
    Rev: server/etc/include/version.h:1.28

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -2,6 +2,8 @@
  * Version information
  */
 
+// Note that version information (major and minor) is also
+// present in module.h.
 constant __roxen_version__ = "2.0";
 constant __roxen_build__ = "23";
 
