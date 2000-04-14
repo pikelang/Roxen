8a21fa0dde25cd9707afdc2521d489ee4b45d01a
Per Hedbor <ph@opera.com>
2000-04-15 00:02:23 +0200

    2.0.45 -> 2.1.0
    Rev: server/etc/include/version.h:1.51

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,8 +4,8 @@
 
 // Note that version information (major and minor) is also
 // present in module.h.
-constant __roxen_version__ = "2.0";
-constant __roxen_build__ = "45";
+constant __roxen_version__ = "2.1";
+constant __roxen_build__ = "0";
 
 #ifdef __NT__
 constant real_version= "Roxen/"+__roxen_version__+"."+__roxen_build__+" NT";
