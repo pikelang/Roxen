02f54aef86be583e142a3324c04e19152a8ccf7a
Peter Bortas <zino@lysator.liu.se>
2000-07-04 20:33:43 +0200

    Version bumped to 2.0.66 by buildsystem.
    Rev: server/etc/include/version.h:1.71

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -5,7 +5,7 @@
 // Note that version information (major and minor) is also
 // present in module.h.
 constant __roxen_version__ = "2.0";
-constant __roxen_build__ = "65";
+constant __roxen_build__ = "66";
 
 #ifdef __NT__
 constant real_version= "Roxen/"+__roxen_version__+"."+__roxen_build__+" NT";
