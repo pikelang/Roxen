28a0806b3b3bfb015585f7d6d4c7f42e1eb39751
Martin Stjernholm <mast@lysator.liu.se>
2000-08-14 20:54:20 +0200

    Added request tracing to the debug log. Use the REQUEST_TRACE define for it.
    Rev: server/etc/include/request_trace.h:1.4

--- server/etc/include/request_trace.h
+++ server/etc/include/request_trace.h
@@ -1,14 +1,26 @@
-#if 1
+// -*- pike -*-
+//
+// Some stuff to do logging of a request through the server.
+//
+// $Id: request_trace.h,v 1.4 2000/08/14 18:54:20 mast Exp $
+
+#ifdef REQUEST_TRACE
+
+# define TRACE_ENTER(A,B) Roxen->trace_enter (id, (A), (B))
+# define TRACE_LEAVE(A) Roxen->trace_leave (id, (A))
+
+#else
+
 # define TRACE_ENTER(A,B) do{ \
-    if(([mapping(string:mixed)]id->misc)->trace_enter) \
-      ([function(string,mixed ...:void)]([mapping(string:mixed)]id->misc)->trace_enter)((A),(B)); \
+    if(function(string,mixed ...:void) _trace_enter = \
+       [function(string,mixed ...:void)]([mapping(string:mixed)]id->misc)->trace_enter) \
+      _trace_enter ((A), (B)); \
   }while(0)
 
 # define TRACE_LEAVE(A) do{ \
-    if(([mapping(string:mixed)]id->misc)->trace_leave) \
-      ([function(string:void)]([mapping(string:mixed)]id->misc)->trace_leave)((A)); \
+    if(function(string:void) _trace_leave = \
+       [function(string:void)]([mapping(string:mixed)]id->misc)->trace_leave) \
+      _trace_leave (A); \
   }while(0)
-#else
-# define TRACE_ENTER(A,B)
-# define TRACE_LEAVE(A)
+
 #endif
