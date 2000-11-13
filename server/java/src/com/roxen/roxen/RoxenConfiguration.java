bcc8e31f1d8dfe0cafb8230346075e9b7de4f09c
Tom Palmer <tomp@uk.uu.net>
2000-11-13 19:34:22 +0000

    Java patches by Tom Palmer applied. Provider module fix.
    Rev: server/java/src/com/roxen/roxen/RoxenConfiguration.java:1.5

--- server/java/src/com/roxen/roxen/RoxenConfiguration.java
+++ server/java/src/com/roxen/roxen/RoxenConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * $Id: RoxenConfiguration.java,v 1.4 2000/02/21 18:30:46 marcus Exp $
+ * $Id: RoxenConfiguration.java,v 1.5 2000/11/13 19:34:22 marcus Exp $
  *
  */
 
@@ -14,6 +14,28 @@ package com.roxen.roxen;
  */
 
 public class RoxenConfiguration {
+  /**
+   * Get the real path of a virtual file
+   *
+   * @param name The virtual file name
+   */
+  public native String getRealPath(String filename, RoxenRequest id);
+
+  /**
+   * Gets the contents of a file
+   *
+   * @param name The name of the file
+   * @return File contents, or null if the file could not be read
+   */
+  public native String getFileContents(String filename, RoxenRequest id);
+
+  /**
+   * Gets the mime type of a file
+   *
+   * @param name The name of the file
+   * @return The mime type of the file
+   */
+  public native String getMimeType(String filename);
 
   /**
    * Get the current value of a global configuration variable
@@ -53,6 +75,24 @@ public class RoxenConfiguration {
   public String queryInternalLocation()
   {
     return queryInternalLocation(null);
+  }
+
+  /**
+   * Returns a list of modules providing a particular service
+   *
+   * @return    the modules that provide this service
+   */
+  public native Module[] getProviders(String provides);
+
+  /**
+   * Returns any module providing a particular service
+   *
+   * @return    the module that provide this service, or null
+   */
+  public Module getProvider(String provides)
+  {
+    Module[] modules = getProviders(provides);
+    return (modules.length>0? modules[0] : null);
   }
 
 }
