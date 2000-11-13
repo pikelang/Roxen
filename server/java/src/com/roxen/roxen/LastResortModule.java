3e20b347ead60dcd2f3f409ac528359ca9fbe21e
Tom Palmer <tomp@uk.uu.net>
2000-11-13 22:34:37 +0000

    Java patches by Tom Palmer applied.
    Rev: server/java/src/com/roxen/roxen/LastResortModule.java:1.1
    Rev: server/java/src/com/roxen/roxen/RoxenClassLoader.java:1.1

---
+++ server/java/src/com/roxen/roxen/LastResortModule.java
@@ -0,0 +1,14 @@
+package com.roxen.roxen;
+
+/**
+ * @author <a href="mailto:tomp@uk.uu.net">Tom Palmer</a>
+ */
+public interface LastResortModule {
+  /**
+   * This method is called when all previous modules have failed to return a response.
+   *
+   * @param id Request Information object associated with the request.
+   * @return null if you didn't handle the request, otherwise your result
+   */
+  public RoxenResponse last_resort(RoxenRequest id);
+}
