c8962cda7b3e6364ba7f1624fcb66debc9e01687
Per Hedbor <ph@opera.com>
2001-02-02 12:23:18 +0100

    Added $
    Rev: server/etc/include/version.h:1.232

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -1,7 +1,6 @@
-/*
- * Version information
- */
-
+// Version information
+// $Id: version.h,v 1.232 2001/02/02 11:23:18 per Exp $
+// 
 // Note that version information (major and minor) is also
 // present in module.h.
 constant __roxen_version__ = "2.2";
