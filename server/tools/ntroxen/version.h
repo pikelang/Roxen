36752b5ad973e028243f8eb603fdf7c9bf481aab
Tomas Nilsson <tomas@roxen.com>
2001-06-20 13:40:34 +0200

    Version bumped to 2.2.18 by buildsystem.
    Rev: server/tools/ntroxen/version.h:1.3

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -1,12 +1,12 @@
 // Version information
-// $Id: version.h,v 1.2 2001/06/18 16:38:43 tomas Exp $
+// $Id: version.h,v 1.3 2001/06/20 11:40:34 tomas Exp $
 // 
 
 #define COPYRIGHT "Copyright (C) 2000-2001 Roxen Internet Software AB"
 
 #define NTSTART_MAJOR_VERSION 2
 #define NTSTART_MINOR_VERSION 2
-#define NTSTART_BUILD_VERSION 17
+#define NTSTART_BUILD_VERSION 18
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
