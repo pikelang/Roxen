220e9dd3e03d2a39a992fab1ed43cb77e7096584
Tomas Nilsson <tomas@roxen.com>
2001-07-30 18:13:37 +0200

    *** empty log message ***
    Rev: server/tools/ntroxen/version.h:1.5

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -1,12 +1,12 @@
 // Version information
-// $Id: version.h,v 1.4 2001/06/27 16:26:36 tomas Exp $
+// $Id: version.h,v 1.5 2001/07/30 16:13:37 tomas Exp $
 // 
 
 #define COPYRIGHT "Copyright (C) 2000-2001 Roxen Internet Software AB"
 
 #define NTSTART_MAJOR_VERSION 2
 #define NTSTART_MINOR_VERSION 2
-#define NTSTART_BUILD_VERSION 25
+#define NTSTART_BUILD_VERSION 31
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
