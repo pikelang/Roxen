7d4540fb1b9f0977e3ab62049679c9f89720c9af
Build system <distmaker@roxen.com>
2001-10-16 11:43:43 +0200

    Version bumped to 2.2.230 by buildsystem.
    Rev: server/etc/include/version.h:1.420

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -1,10 +1,10 @@
 // Version information
-// $Id: version.h,v 1.419 2001/10/15 14:53:19 distmaker Exp $
+// $Id: version.h,v 1.420 2001/10/16 09:43:43 distmaker Exp $
 // 
 // Note that version information (major and minor) is also
 // present in module.h.
 constant __roxen_version__ = "2.2";
-constant __roxen_build__ = "229";
+constant __roxen_build__ = "230";
 
 #ifdef __NT__
 constant real_version= "Roxen/"+__roxen_version__+"."+__roxen_build__+" NT";
