f68c4d7d5084121b3de231202c3a173a222b3820
Tomas Nilsson <tomas@roxen.com>
2001-11-28 12:29:47 +0100

    Added missing create on directories logs and logs/default.
    Rev: server/tools/ntroxen/startdll/roxen.cpp:1.11
    Rev: server/tools/ntroxen/version.h:1.15

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -1,12 +1,12 @@
 // Version information
-// $Id: version.h,v 1.14 2001/11/14 16:29:44 tomas Exp $
+// $Id: version.h,v 1.15 2001/11/28 11:29:34 tomas Exp $
 // 
 
 #define COPYRIGHT "Copyright (C) 2000-2001 Roxen Internet Software AB"
 
 #define NTSTART_MAJOR_VERSION 2
-#define NTSTART_MINOR_VERSION 2
-#define NTSTART_BUILD_VERSION 122
+#define NTSTART_MINOR_VERSION 4
+#define NTSTART_BUILD_VERSION 1
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
