ab8829742a8a1cfa405ce319103db5802ab4c0fe
Tomas Nilsson <tomas@roxen.com>
2002-02-05 14:25:28 +0100

    New major 2.4.
    Rev: server/tools/ntroxen/VERSION:1.123
    Rev: server/tools/ntroxen/version.h:1.16

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -1,12 +1,12 @@
 // Version information
-// $Id: version.h,v 1.15 2001/11/28 11:29:34 tomas Exp $
+// $Id: version.h,v 1.16 2002/02/05 13:25:28 tomas Exp $
 // 
 
 #define COPYRIGHT "Copyright (C) 2000-2001 Roxen Internet Software AB"
 
 #define NTSTART_MAJOR_VERSION 2
 #define NTSTART_MINOR_VERSION 4
-#define NTSTART_BUILD_VERSION 1
+#define NTSTART_BUILD_VERSION 2
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
