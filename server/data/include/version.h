53dcdd8908b6996acfba7f3ae2ce7badb8f5a5c1
Martin Nilsson <mani@lysator.liu.se>
2002-06-14 18:31:51 +0200

    Updated the version number and faked the name until we know the new real name.
    Rev: server/data/include/version.h:1.539

--- server/data/include/version.h
+++ server/data/include/version.h
@@ -1,17 +1,17 @@
 // Version information
-// $Id: version.h,v 1.538 2002/05/30 15:24:43 distmaker Exp $
+// $Id: version.h,v 1.539 2002/06/14 16:31:51 nilsson Exp $
 // 
 // Note that version information (major and minor) is also
 // present in module.h.
-constant __roxen_version__ = "2.4";
-constant __roxen_build__ = "107";
+constant __roxen_version__ = "2.5";
+constant __roxen_build__ = "1";
 
 #if !constant(roxen_release)
 constant roxen_release = "-cvs";
 #endif /* !constant(roxen_release) */
 
 #ifdef __NT__
-constant real_version= "Roxen/"+__roxen_version__+"."+__roxen_build__+" NT"+roxen_release;
+constant real_version= "IS/"+__roxen_version__+"."+__roxen_build__+" NT"+roxen_release;
 #else
-constant real_version= "Roxen/"+__roxen_version__+"."+__roxen_build__+roxen_release;
+constant real_version= "IS/"+__roxen_version__+"."+__roxen_build__+roxen_release;
 #endif
