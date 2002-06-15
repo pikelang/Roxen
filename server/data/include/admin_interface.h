0d910b2b30d8028cb0141d0ad0f5a050242b4359
Martin Nilsson <mani@lysator.liu.se>
2002-06-15 22:29:13 +0200

    Removed unused defines.
    Rev: server/data/include/admin_interface.h:1.11

--- server/data/include/admin_interface.h
+++ server/data/include/admin_interface.h
@@ -1,7 +1,5 @@
 #define config_setting(X) (id->misc->config_settings->query(X,1))
 #define config_setting2(X) RXML.get_var(X,"usr")
 #define config_perm(X)    (id->misc->config_user && id->misc->config_user->auth(X))
-#define config_host       id->misc->remote_config_host
 
 #define SITE_TEMPLATES "site_templates/"
-#define ACTIONS        "config_interface/actions/"
