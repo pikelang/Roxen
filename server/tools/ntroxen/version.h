d67331a2189a46854f744ce92eda33f74ab6809d
Martin Nilsson <mani@lysator.liu.se>
2002-09-26 23:56:59 +0200

    Sync version with Roxen IS version.
    Rev: server/tools/ntroxen/version.h:1.18

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -1,12 +1,12 @@
 // Version information
-// $Id: version.h,v 1.17 2002/04/12 08:54:16 tomas Exp $
+// $Id: version.h,v 1.18 2002/09/26 21:56:59 nilsson Exp $
 // 
 
 #define COPYRIGHT "Copyright (C) 2000-2001 Roxen Internet Software AB"
 
 #define NTSTART_MAJOR_VERSION 2
 #define NTSTART_MINOR_VERSION 4
-#define NTSTART_BUILD_VERSION 46
+#define NTSTART_BUILD_VERSION 74
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
