a5cbec0eecc9f6f4cbe1c82d72a3dc723be4c9d0
Tomas Nilsson <tomas@roxen.com>
2002-09-26 10:47:59 +0200

    Print error message when registry information is missing.
    Rev: server/tools/ntroxen/VERSION:1.196
    Rev: server/tools/ntroxen/startdll/startdll.cpp:1.13
    Rev: server/tools/ntroxen/version.h:1.18

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -1,12 +1,12 @@
 // Version information
-// $Id: version.h,v 1.17 2002/04/12 08:54:16 tomas Exp $
+// $Id: version.h,v 1.18 2002/09/26 08:47:54 tomas Exp $
 // 
 
 #define COPYRIGHT "Copyright (C) 2000-2001 Roxen Internet Software AB"
 
-#define NTSTART_MAJOR_VERSION 2
-#define NTSTART_MINOR_VERSION 4
-#define NTSTART_BUILD_VERSION 46
+#define NTSTART_MAJOR_VERSION 3
+#define NTSTART_MINOR_VERSION 3
+#define NTSTART_BUILD_VERSION 2
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
