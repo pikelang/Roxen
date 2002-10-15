968f08ce0f7b19336ce13508784ffb5735a5e0c2
Tomas Nilsson <tomas@roxen.com>
2002-10-15 11:46:36 +0200

    Don't shutdown already running mysqls if we fail to open any ports. Fixes [Bug 3267].
    Rev: server/tools/ntroxen/startdll/cmdline.h:1.12
    Rev: server/tools/ntroxen/startdll/startdll.cpp:1.14
    Rev: server/tools/ntroxen/version.h:1.20

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -1,12 +1,12 @@
 // Version information
-// $Id: version.h,v 1.19 2002/10/07 08:43:13 tomas Exp $
+// $Id: version.h,v 1.20 2002/10/15 09:46:31 tomas Exp $
 // 
 
 #define COPYRIGHT "Copyright (C) 2000-2001 Roxen Internet Software AB"
 
 #define NTSTART_MAJOR_VERSION 3
 #define NTSTART_MINOR_VERSION 3
-#define NTSTART_BUILD_VERSION 3
+#define NTSTART_BUILD_VERSION 8
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
