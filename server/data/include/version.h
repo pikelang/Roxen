50c1ed6145bd397df748a0834cc114b329959f3c
Martin Nilsson <mani@lysator.liu.se>
2002-11-17 19:33:03 +0100

    Improved, I hope.
    Rev: server/data/include/version.h:1.542

--- server/data/include/version.h
+++ server/data/include/version.h
@@ -1,17 +1,25 @@
 // Version information
-// $Id: version.h,v 1.541 2002/10/23 17:08:09 nilsson Exp $
+// $Id: version.h,v 1.542 2002/11/17 18:33:03 mani Exp $
 // 
 // Note that version information (major and minor) is also
 // present in module.h.
-constant __roxen_version__ = "2002";
-constant __roxen_build__ = "1";
 
-#if !constant(roxen_release)
-constant roxen_release = "-cvs";
-#endif /* !constant(roxen_release) */
+constant __chilimoon_version__ = "2002";
+constant __chilimoon_build__ = "1";
+
+// NGSERVER Remove these
+constant __roxen_version__ = __chilimoon_version__;
+constant __roxen_build__ = __chilimoon_build__;
 
 #ifdef __NT__
-constant real_version= "ChiliMoon/"+__roxen_version__+"."+__roxen_build__+" NT"+roxen_release;
+#define NTRELEASE "-NT"
 #else
-constant real_version= "ChiliMoon/"+__roxen_version__+"."+__roxen_build__+roxen_release;
+#define NTRELEASE
 #endif
+
+#if !constant(chilimoon_release)
+constant chilimoon_release = "-cvs" NTRELEASE;
+#endif /* !constant(chilimoon_release) */
+
+constant real_version = "ChiliMoon/"+__chilimoon_version__+"."+
+  __chilimoon_build__+chilimoon_release;
