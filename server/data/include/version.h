ad66563b52768c53287fc7891d2da78daee1c128
Martin Nilsson <mani@lysator.liu.se>
2003-01-14 20:39:59 +0100

    2002 -> 2003
    Rev: server/data/include/version.h:1.543

--- server/data/include/version.h
+++ server/data/include/version.h
@@ -1,10 +1,10 @@
 // Version information
-// $Id: version.h,v 1.542 2002/11/17 18:33:03 mani Exp $
+// $Id: version.h,v 1.543 2003/01/14 19:39:59 mani Exp $
 // 
 // Note that version information (major and minor) is also
 // present in module.h.
 
-constant __chilimoon_version__ = "2002";
+constant __chilimoon_version__ = "2003";
 constant __chilimoon_build__ = "1";
 
 // NGSERVER Remove these
