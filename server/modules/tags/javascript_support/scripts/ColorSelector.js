21e47f90f9da5803065b47235ae61ade24278229
Jonas Wallden <jonasw@roxen.com>
2004-03-13 17:18:23 +0100

    Added client-side conversion of HTML color names to RGB values. Needed for avoiding CSS style errors when user inputs unknown name, and also because web browsers and Pike don't interpret all names the same (e.g. "green" gives different color values). Implemented CSS-based crosshair to indicate current color selection which also means fewer requests for /internal-roxen-colorbar from the server.
    Rev: server/modules/tags/javascript_support/scripts/ColorSelector.js:1.3

--- server/modules/tags/javascript_support/scripts/ColorSelector.js
+++ server/modules/tags/javascript_support/scripts/ColorSelector.js
@@ -1,3 +1,91 @@
+//  Color selector scripts. Used by <var type="color"> in WebServer wizards.
+//  $Id: ColorSelector.js,v 1.3 2004/03/13 16:18:23 jonasw Exp $
+
+
+//  Known HTML color names
+var html_color_names = new Array(
+      "ALICEBLUE", "ANTIQUEWHITE", "AQUA", "AQUAMARINE", "AZURE", "BEIGE",
+      "BISQUE", "BLACK", "BLANCHEDALMOND", "BLUE", "BLUEVIOLET", "BROWN",
+      "BURLYWOOD", "CADETBLUE", "CHARTREUSE", "CHOCOLATE", "CORAL",
+      "CORNFLOWERBLUE", "CORNSILK", "CRIMSON", "CYAN", "DARKBLUE",
+      "DARKCYAN", "DARKGOLDENROD", "DARKGRAY", "DARKGREEN", "DARKKHAKI",
+      "DARKMAGENTA", "DARKOLIVEGREEN", "DARKORANGE", "DARKORCHID",
+      "DARKRED", "DARKSALMON", "DARKSEAGREEN", "DARKSLATEBLUE",
+      "DARKSLATEGRAY", "DARKTURQUOISE", "DARKVIOLET", "DEEPPINK",
+      "DEEPSKYBLUE", "DIMGRAY", "DODGERBLUE", "FELDSPAR", "FIREBRICK",
+      "FLORALWHITE", "FORESTGREEN", "FUCHSIA", "GAINSBORO", "GHOSTWHITE",
+      "GOLD", "GOLDENROD", "GRAY", "GREEN", "GREENYELLOW", "HONEYDEW",
+      "HOTPINK", "INDIANRED ", "INDIGO ", "IVORY", "KHAKI", "LAVENDER",
+      "LAVENDERBLUSH", "LAWNGREEN", "LEMONCHIFFON", "LIGHTBLUE",
+      "LIGHTCORAL", "LIGHTCYAN", "LIGHTGOLDENRODYELLOW", "LIGHTGREY",
+      "LIGHTGREEN", "LIGHTPINK", "LIGHTSALMON", "LIGHTSEAGREEN",
+      "LIGHTSKYBLUE", "LIGHTSLATEBLUE", "LIGHTSLATEGRAY", "LIGHTSTEELBLUE",
+      "LIGHTYELLOW", "LIME", "LIMEGREEN", "LINEN", "MAGENTA", "MAROON",
+      "MEDIUMAQUAMARINE", "MEDIUMBLUE", "MEDIUMORCHID", "MEDIUMPURPLE",
+      "MEDIUMSEAGREEN", "MEDIUMSLATEBLUE", "MEDIUMSPRINGGREEN",
+      "MEDIUMTURQUOISE", "MEDIUMVIOLETRED", "MIDNIGHTBLUE", "MINTCREAM",
+      "MISTYROSE", "MOCCASIN", "NAVAJOWHITE", "NAVY", "OLDLACE", "OLIVE",
+      "OLIVEDRAB", "ORANGE", "ORANGERED", "ORCHID", "PALEGOLDENROD",
+      "PALEGREEN", "PALETURQUOISE", "PALEVIOLETRED", "PAPAYAWHIP",
+      "PEACHPUFF", "PERU", "PINK", "PLUM", "POWDERBLUE", "PURPLE", "RED",
+      "ROSYBROWN", "ROYALBLUE", "SADDLEBROWN", "SALMON", "SANDYBROWN",
+      "SEAGREEN", "SEASHELL", "SIENNA", "SILVER", "SKYBLUE", "SLATEBLUE",
+      "SLATEGRAY", "SNOW", "SPRINGGREEN", "STEELBLUE", "TAN", "TEAL",
+      "THISTLE", "TOMATO", "TURQUOISE", "VIOLET", "VIOLETRED", "WHEAT",
+      "WHITE", "WHITESMOKE", "YELLOW", "YELLOWGREEN");
+
+var html_color_values = new Array(
+      "#F0F8FF", "#FAEBD7", "#00FFFF", "#7FFFD4", "#F0FFFF", "#F5F5DC",
+      "#FFE4C4", "#000000", "#FFEBCD", "#0000FF", "#8A2BE2", "#A52A2A",
+      "#DEB887", "#5F9EA0", "#7FFF00", "#D2691E", "#FF7F50", "#6495ED",
+      "#FFF8DC", "#DC143C", "#00FFFF", "#00008B", "#008B8B", "#B8860B",
+      "#A9A9A9", "#006400", "#BDB76B", "#8B008B", "#556B2F", "#FF8C00",
+      "#9932CC", "#8B0000", "#E9967A", "#8FBC8F", "#483D8B", "#2F4F4F",
+      "#00CED1", "#9400D3", "#FF1493", "#00BFFF", "#696969", "#1E90FF",
+      "#D19275", "#B22222", "#FFFAF0", "#228B22", "#FF00FF", "#DCDCDC",
+      "#F8F8FF", "#FFD700", "#DAA520", "#808080", "#008000", "#ADFF2F",
+      "#F0FFF0", "#FF69B4", "#CD5C5C", "#4B0082", "#FFFFF0", "#F0E68C",
+      "#E6E6FA", "#FFF0F5", "#7CFC00", "#FFFACD", "#ADD8E6", "#F08080",
+      "#E0FFFF", "#FAFAD2", "#D3D3D3", "#90EE90", "#FFB6C1", "#FFA07A",
+      "#20B2AA", "#87CEFA", "#8470FF", "#778899", "#B0C4DE", "#FFFFE0",
+      "#00FF00", "#32CD32", "#FAF0E6", "#FF00FF", "#800000", "#66CDAA",
+      "#0000CD", "#BA55D3", "#9370D8", "#3CB371", "#7B68EE", "#00FA9A",
+      "#48D1CC", "#C71585", "#191970", "#F5FFFA", "#FFE4E1", "#FFE4B5",
+      "#FFDEAD", "#000080", "#FDF5E6", "#808000", "#6B8E23", "#FFA500",
+      "#FF4500", "#DA70D6", "#EEE8AA", "#98FB98", "#AFEEEE", "#D87093",
+      "#FFEFD5", "#FFDAB9", "#CD853F", "#FFC0CB", "#DDA0DD", "#B0E0E6",
+      "#800080", "#FF0000", "#BC8F8F", "#4169E1", "#8B4513", "#FA8072",
+      "#F4A460", "#2E8B57", "#FFF5EE", "#A0522D", "#C0C0C0", "#87CEEB",
+      "#6A5ACD", "#708090", "#FFFAFA", "#00FF7F", "#4682B4", "#D2B48C",
+      "#008080", "#D8BFD8", "#FF6347", "#40E0D0", "#EE82EE", "#D02090",
+      "#F5DEB3", "#FFFFFF", "#F5F5F5", "#FFFF00", "#9ACD32");
+
+
+//  Validation of HTML color names
+function html_color_to_rgb(name)
+{
+  name = name.toUpperCase();
+  var start = 0;
+  var end = html_color_names.length - 1;
+  while (end - start > 3) {
+    var pos = Math.floor((start + end) / 2);
+    var test = html_color_names[pos];
+    if (test == name)
+      return html_color_values[pos];
+    else if (test < name)
+      start = pos + 1;
+    else
+      end = pos - 1;
+  }
+  for (var pos = start; pos <= end; pos++) {
+    if (html_color_names[pos] == name)
+      return html_color_values[pos];
+  }
+  return "#000000";
+}
+
+
+
 //  Conversion from integer to hex
 var hex_digits = new Array("0", "1", "2", "3", "4", "5", "6", "7",
 			   "8", "9", "A", "B", "C", "D", "E", "F");
@@ -60,16 +148,10 @@ function hsv_to_rgb_string(h, s, v)
 
 function rgb_string_to_hsv(rgb_str)
 {
-  //  Pad to at least 6 digits and strip # prefix
-  rgb_str = rgb_str + "000000";
-  if (rgb_str.charAt(0) == "#")
-    rgb_str = rgb_str.substring(1, 7);
-  else
-    rgb_str = rgb_str.substring(0, 6);
-  
-  var r = hex_to_int(rgb_str.substring(0, 2));
-  var g = hex_to_int(rgb_str.substring(2, 4));
-  var b = hex_to_int(rgb_str.substring(4, 6));
+  //  Expected input is on form "#rrggbb"
+  var r = hex_to_int(rgb_str.substring(1, 3));
+  var g = hex_to_int(rgb_str.substring(3, 5));
+  var b = hex_to_int(rgb_str.substring(5, 7));
   var min, max, delta;
   var h, s, v;
 
@@ -99,7 +181,7 @@ function rgb_string_to_hsv(rgb_str)
 }
 
 
-function colsel_click(event, prefix, h, s, v, in_bar)
+function colsel_click(event, prefix, h, s, v, in_bar, in_cross)
 {
   var x = (event.clientX - getTargetX(event)) * 2;
   var y = (event.clientY - getTargetY(event)) * 2;
@@ -109,7 +191,13 @@ function colsel_click(event, prefix, h, s, v, in_bar)
   if (y < 0) y = 0;
   if (y > 255) y = 255;
 
-  if (in_bar) {
+  if (in_cross) {
+    if (in_cross == "x") {
+      v = 255 - y;
+    } else if (in_cross == "y") {
+      h = x;
+    }
+  } else if (in_bar) {
     s = 255 - y;
   } else {
     h = x;
@@ -126,9 +214,19 @@ function colsel_update(prefix, h, s, v, update_field, force_color)
     
   var bar_img = getObject(prefix + "colorbar");
   if (bar_img) {
-    var bar_url = "/internal-roxen-colorbar-small:" + h + "," + v + "," + s;
+    var bar_url = "/internal-roxen-colorbar-small:" + h + "," + v + ",-1";
     bar_img.src = bar_url;
   }
+
+  var mark_x_img = getObject(prefix + "mark_x");
+  if (mark_x_img)
+    mark_x_img.style.left = 5 + Math.floor(h / 2);
+  var mark_y_img = getObject(prefix + "mark_y");
+  if (mark_y_img)
+    mark_y_img.style.top = 5 + Math.floor((255 - v) / 2);
+  var mark_y_small_img = getObject(prefix + "mark_y_small");
+  if (mark_y_small_img)
+    mark_y_small_img.style.top = 5 + Math.floor((255 - s) / 2);
   
   var preview_td = getObject(prefix + "preview");
   if (preview_td) {
@@ -144,12 +242,26 @@ function colsel_update(prefix, h, s, v, update_field, force_color)
 }
 
 
+function normalize_rgb_string(rgb_str)
+{
+  //  Pad to at least 6 digits and convert to uppercase
+  rgb_str = rgb_str + "000000";
+  rgb_str = rgb_str.substring(0, 7);
+  return rgb_str.toUpperCase();
+}
+
+
 function colsel_type(prefix, value, update_field)
 {
+  //  If value starts with # we consider it to be #RRGGBB format
+  var is_hex = (value.charAt(0) == "#");
+  var rgb_value =
+    is_hex ? normalize_rgb_string(value) : html_color_to_rgb(value);
+  
   //  Attempt to parse the color as RRGGBB and set the popup images
   //  accordingly.
-  var new_hsv = rgb_string_to_hsv(value);
+  var new_hsv = rgb_string_to_hsv(rgb_value);
   colsel_update(prefix, new_hsv[0], new_hsv[1], new_hsv[2],
-                update_field, value);
+                update_field, rgb_value);
   return new_hsv;
 }
