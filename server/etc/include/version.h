98e7b7e1222e809aff921a8b12d9da6c52c080b2
Henrik Grubbström (Grubba) <grubba@grubba.org>
2004-08-27 14:33:57 +0200

    Reset the build number.
    Rev: server/etc/include/version.h:1.853

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -1,10 +1,10 @@
 // Version information
-// $Id: version.h,v 1.852 2004/08/27 11:55:57 stewa Exp $
+// $Id: version.h,v 1.853 2004/08/27 12:33:57 grubba Exp $
 // 
 // Note that version information (major and minor) is also
 // present in module.h.
 constant __roxen_version__ = "4.1";
-constant __roxen_build__ = "145";
+constant __roxen_build__ = "0";
 
 #if !constant(roxen_release)
 constant roxen_release = "-cvs";
