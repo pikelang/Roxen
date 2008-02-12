96e0f204bf87a45e041c3584c119d70e9ea3608b
Build system <distmaker@roxen.com>
2008-02-12 08:14:11 +0100

    Version bumped to 4.5.236 by buildsystem.
    Rev: server/etc/include/version.h:1.1090

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -1,10 +1,10 @@
 // Version information
-// $Id: version.h,v 1.1089 2008/02/11 16:05:41 dist Exp $
+// $Id: version.h,v 1.1090 2008/02/12 07:14:11 dist Exp $
 // 
 // Note that version information (major and minor) is also
 // present in module.h.
 constant __roxen_version__ = "4.5";
-constant __roxen_build__ = "235";
+constant __roxen_build__ = "236";
 
 #if !constant(roxen_release)
 constant roxen_release = "-cvs";
