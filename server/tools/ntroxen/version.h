c0f5463785d98e840b0718bd12ae1c07ccfa4724
Build system <distmaker@roxen.com>
2008-07-30 00:15:37 +0200

    Version bumped to 4.0.47 by buildsystem.
    Rev: server/tools/ntroxen/version.h:1.25

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -1,12 +1,12 @@
 // Version information
-// $Id: version.h,v 1.24 2008/07/29 21:39:14 mast Exp $
+// $Id: version.h,v 1.25 2008/07/29 22:15:37 dist Exp $
 // 
 
 #define COPYRIGHT "Copyright (C) 2000-2008 Roxen Internet Software AB"
 
 #define NTSTART_MAJOR_VERSION 4
 #define NTSTART_MINOR_VERSION 0
-#define NTSTART_BUILD_VERSION 46
+#define NTSTART_BUILD_VERSION 47
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
