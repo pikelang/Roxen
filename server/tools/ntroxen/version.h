10fc2babb22d72d13978aba34f5bb3d4b67c4696
Martin Stjernholm <mast@lysator.liu.se>
2008-07-29 23:39:14 +0200

    Bumped version to get back in synch with VERSION file.
    Rev: server/tools/ntroxen/version.h:1.24

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -1,12 +1,12 @@
 // Version information
-// $Id: version.h,v 1.23 2008/07/29 21:31:56 mast Exp $
+// $Id: version.h,v 1.24 2008/07/29 21:39:14 mast Exp $
 // 
 
 #define COPYRIGHT "Copyright (C) 2000-2008 Roxen Internet Software AB"
 
-#define NTSTART_MAJOR_VERSION 3
-#define NTSTART_MINOR_VERSION 3
-#define NTSTART_BUILD_VERSION 17
+#define NTSTART_MAJOR_VERSION 4
+#define NTSTART_MINOR_VERSION 0
+#define NTSTART_BUILD_VERSION 46
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
