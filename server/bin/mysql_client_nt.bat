6ac3b24f729892c8a5efb334ef50951c4405ad2f
Martin Stjernholm <mast@lysator.liu.se>
2009-05-14 02:37:41 +0200

    Added a fixme about using mysql-location.txt.
    Rev: server/bin/mysql_client_nt.bat:1.2

--- server/bin/mysql_client_nt.bat
+++ server/bin/mysql_client_nt.bat
@@ -4,4 +4,6 @@ echo %~dp0 | "%~dp0..\pike\bin\pike" -e "string dir = Stdio.stdin.read(); string
 set /p MYSQL_PIPE= < mysql_pipe.txt
 del mysql_pipe.txt
 
+rem FIXME: Use mysql-location.txt to find the binary.
+
 "%~dp0..\mysql\bin\mysql" -urw --pipe --socket="%MYSQL_PIPE%"
