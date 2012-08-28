e658dba52f25cfc6e4dc24daa9cf82ed22110d53
Build system <distmaker@roxen.com>
2012-08-28 08:59:31 +0200

    Version bumped to 5.2.230 by buildsystem.
    Rev: server/etc/include/version.h:1.1761

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -1,10 +1,10 @@
 // Version information
-// $Id: version.h,v 1.1760 2012/08/27 10:49:31 dist Exp $
+// $Id: version.h,v 1.1761 2012/08/28 06:59:31 dist Exp $
 // 
 // Note that version information (major and minor) is also
 // present in module.h.
 constant roxen_ver = "5.2";
-constant roxen_build = "229";
+constant roxen_build = "230";
 
 #if !constant(roxen_release)
 constant roxen_release = "-cvs";
