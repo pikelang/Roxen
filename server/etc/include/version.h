0201a3e20f1043017940a7ab0c08a4b81eba00c6
Build system <distmaker@roxen.com>
2013-05-15 16:09:45 +0200

    Version bumped to 5.2.362 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // Note that version information (major and minor) is also
 // present in module.h.
 constant roxen_ver = "5.2";
-constant roxen_build = "361";
+constant roxen_build = "362";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
