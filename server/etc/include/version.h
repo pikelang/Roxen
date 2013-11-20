64ea9f116367427932716bbbb417bd3f20b99feb
Henrik Grubbström (Grubba) <grubba@grubba.org>
2013-11-20 12:04:08 +0100

    Roxen 5.4.1.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -2,8 +2,8 @@
 // 
 // Note that version information (major and minor) is also
 // present in module.h.
-constant roxen_ver = "5.2";
-constant roxen_build = "335";
+constant roxen_ver = "5.4";
+constant roxen_build = "1";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
