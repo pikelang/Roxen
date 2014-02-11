32bf8d338724c0113138fe528b8cf6cc475783c0
Henrik Grubbström (Grubba) <grubba@grubba.org>
2014-02-11 09:53:43 +0100

    ntstart: Bumped version to 5.4.0.
    Needed to avoid version conflicts between the ntstarts built on
    branches 5.2 and 5.4.

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -4,9 +4,9 @@
 
 #define COPYRIGHT "Copyright (C) 2000-2009 Roxen Internet Software AB"
 
-#define NTSTART_MAJOR_VERSION 4
-#define NTSTART_MINOR_VERSION 1
-#define NTSTART_BUILD_VERSION 34
+#define NTSTART_MAJOR_VERSION 5
+#define NTSTART_MINOR_VERSION 4
+#define NTSTART_BUILD_VERSION 0
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
