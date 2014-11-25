2ab09f1d7a449000a6f8b0b1e1c009160f08e51f
Martin Karlgren <marty@roxen.com>
2014-11-25 11:47:17 +0100

    Version bumped to 6.0.9 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
 constant roxen_ver = "6.0";
-constant roxen_build = "8";
+constant roxen_build = "9";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
