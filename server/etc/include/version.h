8b2dca5e135bcffd18f1209d58ec4abb2f58044e
Marcus Agehall <agehall@roxen.com>
2014-12-07 16:57:46 +0100

    Version bumped to 5.5.82 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
 constant roxen_ver = "5.5";
-constant roxen_build = "81";
+constant roxen_build = "82";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
