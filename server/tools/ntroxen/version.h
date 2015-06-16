8c6fca357a78c5fa672f7a612dc7a823b9a9ac9a
Henrik Grubbström (Grubba) <grubba@grubba.org>
2015-06-16 17:46:47 +0200

    Development branch bumped to 6.1.0 [ntroxen].

--- server/tools/ntroxen/version.h
+++ server/tools/ntroxen/version.h
@@ -5,8 +5,8 @@
 #define COPYRIGHT "Copyright (C) 2000-2014 Roxen Internet Software AB"
 
 #define NTSTART_MAJOR_VERSION 6
-#define NTSTART_MINOR_VERSION 0
-#define NTSTART_BUILD_VERSION 1
+#define NTSTART_MINOR_VERSION 1
+#define NTSTART_BUILD_VERSION 0
 
 #define STR2(X) #X
 #define STR(X) STR2(X)
