7a13da3b031aa57f2fd9dca8df3ae6d5f14c31ab
Henrik Grubbström (Grubba) <grubba@grubba.org>
2016-10-03 16:29:05 +0200

    Bumped devel version to 6.2.0.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -3,8 +3,8 @@
 // Note that version information (major and minor) is also
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
-constant roxen_ver = "6.1";
-constant roxen_build = "129";
+constant roxen_ver = "6.2";
+constant roxen_build = "0";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
