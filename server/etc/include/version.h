f5b828ee818097decde37c26e631c432ac4d7199
Jonas Wallden <jonasw@roxen.com>
2017-03-07 11:12:58 +0100

    Version bumped to 6.1.201 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
 constant roxen_ver = "6.1";
-constant roxen_build = "200";
+constant roxen_build = "201";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
