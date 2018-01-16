0fa04120a7c71b275512f4c13bfeac0b01b2e349
Pontus Östlund <ponost@roxen.com>
2018-01-16 14:17:43 +0100

    Added macro CFIF_THROW which logs by default but throws if ADMIN_IF_DEBUG is set.

--- server/etc/include/config_interface.h
+++ server/etc/include/config_interface.h
@@ -8,6 +8,8 @@
 
 #ifdef ADMIN_IF_DEBUG
 # define TRACE(X...)werror("%s:%d: %s",basename(__FILE__),__LINE__,sprintf(X))
+# define CFIF_THROW(X...)error("CFIF17: " + sprintf(X))
 #else
 # define TRACE(X...)
+# define CFIF_THROW(X...)werror("__notice__:%s:%d: %s",basename(__FILE__),__LINE__,sprintf(X))
 #endif
