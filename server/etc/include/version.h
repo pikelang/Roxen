2f669525edff1be1d1cf5554df48caa3edc21800
Daniel Wressle <wressle@roxen.com>
2018-05-24 13:59:22 +0200

    Version bumped to 6.2.143 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
 constant roxen_ver = "6.2";
-constant roxen_build = "142";
+constant roxen_build = "143";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
