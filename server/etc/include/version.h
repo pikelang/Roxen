2aae303e976eb6c8bbed37bed6bb629fe8aed9e3
Automatic Build <autobuild@roxen.com>
2019-04-19 01:02:24 +0200

    Version bumped to 6.3.186 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
 constant roxen_ver = "6.3";
-constant roxen_build = "185";
+constant roxen_build = "186";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
