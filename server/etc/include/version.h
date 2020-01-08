b5f779e8d5e5bfff73133e4646bda920cd9f8123
Marcus Wellhardh <wellhardh@roxen.com>
2020-01-08 14:21:06 +0100

    Version bumped to 6.3.407 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
 constant roxen_ver = "6.3";
-constant roxen_build = "406";
+constant roxen_build = "407";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
