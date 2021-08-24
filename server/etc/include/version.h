0c68241983eca698baf799b7f73df3c4f41d4548
Henrik Grubbström (Grubba) <grubba@grubba.org>
2021-08-24 10:17:57 +0200

    Version bumped to 6.3.994 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
 constant roxen_ver = "6.3";
-constant roxen_build = "993";
+constant roxen_build = "994";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
