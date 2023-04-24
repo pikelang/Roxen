5ef4e32ea13db08be4707af3928b5f16533f2ed1
Henrik Grubbström (Grubba) <grubba@grubba.org>
2023-04-24 12:47:05 +0200

    Bump devel version to 7.2.0.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -3,8 +3,8 @@
 // Note that version information (major and minor) is also
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
-constant roxen_ver = "7.1";
-constant roxen_build = "368";
+constant roxen_ver = "7.2";
+constant roxen_build = "0";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
