b4766d3bdfac4a48197b5d21a97249c5595b8914
Daniel Wressle <wressle@roxen.com>
2023-05-22 12:19:09 +0200

    Version bumped to 7.1.407 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
 constant roxen_ver = "7.1";
-constant roxen_build = "406";
+constant roxen_build = "407";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
