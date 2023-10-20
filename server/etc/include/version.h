cde4a8e11dd9a591ddf95b0157f995f74de3023f
Automatic Build <autobuild@roxen.com>
2023-10-20 23:00:49 +0200

    Version bumped to 7.0.722 by buildsystem.

--- server/etc/include/version.h
+++ server/etc/include/version.h
@@ -4,7 +4,7 @@
 // present in module.h and in the roxen.version test in
 // ../test/tests/rxml/RoxenTest_BaseRXML.xml .
 constant roxen_ver = "7.0";
-constant roxen_build = "721";
+constant roxen_build = "722";
 
 #if !constant(roxen_release)
 constant roxen_release = "-git";
